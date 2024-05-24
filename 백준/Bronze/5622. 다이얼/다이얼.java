2925105692f6fd9e34cfe5cc1762310f40783d25
taeyeon0721 <97265231+taeyeon0721@users.noreply.github.com>
2024-05-24 15:26:14 +0900

    [Bronze II] Title: 다이얼, Time: 192 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,51 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+
+	public static void main(String[] args){
+
 		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
+		String S = sc.next();
 		sc.close();
 		
-		int count = 0;
+		int sum = 0;
+		for(int i=0; i<S.length();i++) { 
 		
-		for(int i=0;i<str.length();i++) {
-			switch(str.charAt(i)) {
-			
-			case 'A': case 'B': case 'C':
-				count+=3;
+			switch(S.charAt(i)) {
+			case 'A': case'B': case'C':
+				sum += 3;
 				break;
-			
-			case 'D': case 'E': case 'F':
-				count+=4;
+			case 'D': case'E': case'F':
+				sum += 4;
 				break;
-			
-			case 'G': case 'H': case 'I':
-				count+=5;
+			case 'G': case'H': case'I':
+				sum += 5;
 				break;
-			
-			case 'J': case 'K': case 'L':
-				count+=6;
+			case 'J': case'K': case'L':
+				sum += 6;	
+                 break;
+			case 'M': case'N': case'O':
+				sum += 7;
 				break;
-			
-			case 'M': case 'N': case 'O':
-				count+=7;
+			case 'P': case'Q': case'R': case'S':
+				sum += 8;
 				break;
-			
-			case 'P': case 'Q': case 'R': case 'S':
-				count+=8;
+			case 'T': case'U': case'V':
+				sum += 9;
 				break;
-			
-			case 'T': case 'U': case 'V':
-				count+=9;
+			case 'W': case'X': case'Y': case 'Z':
+				sum += 10;
 				break;
-			
-			case 'W': case 'X': case 'Y': case 'Z':
-				count+=10;
-				break;
-				
-			}	
+			}
 		}
-		System.out.println(count);
+		System.out.println(sum);
 	}
 }

92e88e7e3dbab7c9b4e315d2f4dbc0a0c32cc20a
taeyeon0721 <97265231+taeyeon0721@users.noreply.github.com>
2024-05-25 11:16:12 +0900

    Update A＋B － 5.java

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -10,7 +10,7 @@ public class Main {
 			int A = sc.nextInt();
 			int B = sc.nextInt();
            		if(A == 0 && B == 0) break; //A,B둘다 0이어야하기 때문에 조건 따로 해주기 
-						    -> 처음엔 이렇게 했는데, A+B==0 로 하면 음수 양수로 입력될수도 있으니까 
+						    //-> 처음엔 이렇게 했는데, A+B==0 로 하면 음수 양수로 입력될수도 있으니까 
 			System.out.println(A+B);
 		}
 		sc.close();

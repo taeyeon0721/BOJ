d678e3a0f6c366c37e366e9e08604b62dac0a59a
taeyeon0721 <97265231+taeyeon0721@users.noreply.github.com>
2024-06-01 13:04:15 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,30 +1,30 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		float total = 0;
-		float max = 0;
-		int[] arr = new int[N];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
+		double sum = 0.0; //실수로 선언★
+		int max = -1; //입력받을 값이 0보다 크거나 같기 때문
 		
 		for(int i=0; i<N; i++) {
-			int grade = sc.nextInt();
-			arr[i] = grade;
-			if(grade > max) {
-				max = grade ;
-			}			
+			int grade = Integer.parseInt(st.nextToken());
+			
+			if(grade > max) { //최대값 찾기 
+				max = grade;
+			}
+			
+			sum += grade; //누적산해서 점수의 총합 구해줌
 		}
-		sc.close();
 		
-		for(int i=0; i<arr.length; i++) {
-			total = total + (arr[i]/max)*100;
-		}
-		float avg = total/N;
-		System.out.println(avg);
+		System.out.println((sum/max)*100/N);//sum이 실수이기 때문에 다른 int값 자동 형변환
+		
 	}
 }

c9f440d7531e63d9e876a63fc7d2bb870e062f9f
taeyeon0721 <97265231+taeyeon0721@users.noreply.github.com>
2024-06-02 00:07:19 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int gap = 2541 - 1998;
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		System.out.println(y - gap);
+		sc.close();
+		
+	}
+}

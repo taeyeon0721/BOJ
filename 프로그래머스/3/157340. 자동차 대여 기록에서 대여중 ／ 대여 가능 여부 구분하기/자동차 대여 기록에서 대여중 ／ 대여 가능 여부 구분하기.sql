7c054400df3712c29be782ac1213a93e6bd1e3d1
taeyeon0721 <97265231+taeyeon0721@users.noreply.github.com>
2024-06-14 13:26:34 +0900

    Update 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,4 +1,9 @@
--- 코드를 입력하세요
+-- MAX값이 핵심★
+-- MAX를 쓰지 않으면 CAR_ID당 대여기록이 여러개 있을 때 여러행 반환해버림
+-- GROUP BY CAR_ID별로 MAX값을 구해야 대여중인 경우가 하나라도 있을 때, 대여 가능보다 더 큰 문자열인 대여중을 반환
+
+-- CASE WHEN 사용★
+
 SELECT CAR_ID,
     MAX(CASE 
             WHEN DATE '2022-10-16' BETWEEN START_DATE AND END_DATE THEN '대여중'

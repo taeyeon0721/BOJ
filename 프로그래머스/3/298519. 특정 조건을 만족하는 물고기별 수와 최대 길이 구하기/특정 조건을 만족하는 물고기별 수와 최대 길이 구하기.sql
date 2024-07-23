587819a61c65e9dc7720d9a47cf91e554fab8117
taeyeon0721 <97265231+taeyeon0721@users.noreply.github.com>
2024-07-23 22:53:09 +0900

    Update 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -6,3 +6,8 @@ SELECT COUNT(FISH_TYPE) AS FISH_COUNT
     GROUP BY FISH_TYPE
     HAVING AVG(IFNULL(LENGTH,10)) >= 33
     ORDER BY 3;
+
+-- ★ [IFNULL을 사용하는 이유] ★
+-- "NULL 값이 포함된 컬럼"에서 "평균을 계산할 때 NULL 값은 무시"된다.
+-- 따라서, NULL 값을 <특정 값으로 대체>하여 <평균 계산에 포함>시키고 싶을 때 <IFNULL을 사용>한다. 
+-- 이렇게 하면 NULL 값 때문에 평균이 과도하게 낮아지는 것을 방지할 수 있다.
